3189d80b8cedce248186c9308a9106a1240c1229
meenado89 <meenakshiemail89@gmail.com>
2025-09-22 19:41:02 +0530

    array with user input loop and multiplication

--- array.cpp
+++ array.cpp
@@ -1,22 +1,92 @@
 
 #include <iostream>
 using namespace std;
-
+// 1
 //MULTIDIMENSIONAL LOOPING ARRAY, INT DECLARED DURING INITIALIZATION
-int main()
-{
-    int i;
-    int j;
-    string cars[3] = {"Ca", "Cb", "Cd"};
-    string Car [3] = {"CA", "CB", "CD"};
-    for ( i = 0; i < 3; i++)
-    {
-        for ( j = 0; j <= i; j++)
-        {
-            cout << cars[j]<<" ";
-    }
-    cout << Car[i]<<" "<<endl;
-    }
-    return 0;
-}
+// int main()
+// {
+//     int i;
+//     int j;
+//     string cars[3] = {"Ca", "Cb", "Cd"};
+//     string Car [3] = {"CA", "CB", "CD"};
+//     for ( i = 0; i <= 3; i++)
+//     {
+//         for ( j = 0; j <= i; j++)
+//         {
+//             cout << cars[j]<<" ";
+//     }
+//     cout << Car[i]<<" "<<endl;
+//     }
+//     return 0;
+// }
 
+
+// 2
+// int main (){
+// int i;
+// int j;
+// int a[5][5];
+//  int index;
+// for (int i=0; i<5; i++) {
+//     for (int j=0; j<5; j++)
+    
+//     a[i][j]=a[i-1][j-1]+5;
+     
+//     cout<<"enter values "<<endl;
+//     cin>>a[i][j];
+//     cout<<a[index];
+// }
+// }
+
+// 3
+// prince's code to get loop
+// int main() {
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+   
+//     for (int i = 0; i < 5; i++) {
+      
+//         for (int j = 0; j < 5; j++) {
+//             cout << num << " ";  
+//         }
+//         cout << endl;  
+//     }
+
+//     return 0;
+// }
+
+// 4
+
+// FOR MULTIPLICATION USER INPUT
+
+// int main() {
+//     int num;
+//     cout << "Enter a random number: ";
+//     cin >> num;
+//     cout << "Multiplication table of " << num << ":" << endl;
+//     for (int i = 1; i <= 10; i++) {
+//         cout << num << " x " << i << " = " << num * i << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// 5
+//GETTING VALUE FROM USERS TO PRINT THE SELECTED ARRAY
+// int main (){
+// string random[] = { "apple", "car", "sanjeev", "subject"};
+// int index;
+
+// cout<<"select the String Numer to be Printed (0-3):- ";
+// cin>>index;
+// cout<<random[index];
+
+// return 0;
+
+// }
+
+
+
+ 
