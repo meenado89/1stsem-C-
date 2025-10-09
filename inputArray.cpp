04ea9ca127d7c8e32182cfade3dbf79a33916ae7
meenado89 <meenakshiemail89@gmail.com>
2025-10-09 12:19:01 +0530

    user input for each element

--- inputArray.cpp
+++ inputArray.cpp
@@ -1,25 +1,58 @@
 #include <iostream>
 using namespace std;
 // USER INPUT FOR INDEX AND ONE ELEMENT THEN MULTIPLY
+// int main() {
+//     int index;
+//     int element;
+
+//     cout << "Enter the size of the array: ";
+//     cin >> index;
+
+//     cout << "Enter the starting element: ";
+//     cin >> element;
+
+//     int array[index];
+//     for (int i = 0; i < index; i++) {
+//         array[i] = element * i;
+//     }
+//     for (int i = 0; i < index; i++) {
+//         cout << "array[" << i << "] = " << array[i] << endl;
+//     }
+//     return 0;
+// }
+
+// USER INPUT FOR INDEX AND THEN INPUT FOR EACH ELEMENT(SIRS)
 int main() {
-    int index;    
-    int element;  
+    int rows, cols;
 
-    cout << "Enter the size of the array: ";
-    cin >> index;
+    // Get the size of the array
+    cout << "Enter number of rows: ";
+    cin >> rows;
 
-    cout << "Enter the starting element: ";
-    cin >> element;
+    cout << "Enter number of columns: ";
+    cin >> cols;
 
-    int array[index];
-    for (int i = 0; i < index; i++) {
-        array[i] = element * i; 
-    }   
-    for (int i = 0; i < index; i++) {
-        cout << "array[" << i << "] = " << array[i] << endl;
+    // Declare the 2D array
+    int arr[rows][cols];
+
+    // Input elements
+    cout << "Enter " << rows * cols << " elements:" << endl;
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            cin >> arr[i][j];
+        }
     }
-    return 0;
+
+    // Print the array
+    cout << "The array is:" << endl;
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+       return 0;
 }
 
-// USER INPUT FOR INDEX AND THEN INPUT FOR EACH ELEMENT 
-  
+
